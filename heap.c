cc243affa4c98539309b4f3ace0ab9987f18b80f
Joseph Valer Torres <jvaler@fi.uba.ar>
2022-06-05 03:26:51 -0300

    Se modifico la primitiva de downheap y heap crear para utilizar la misma primitica de heap_crear_arr

--- heap.c
+++ heap.c
@@ -7,8 +7,10 @@
 #define DEBE_ACHICAR 4
 #define MINIMO_PARA_ACHICAR 2
 
-void swap(void* elem1, void* elem2){
-
+void swap(void * valores[],size_t pos_A, pos_B){
+	void *aux = valores[pos_A];
+	valores[pos_A] = valores[pos_B];
+	valores[pos_B] = aux;
 }
 
 void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp){
@@ -27,22 +29,6 @@ struct heap{
     cmp_func_t funcion_comparar;
 };
 
-heap_t *heap_crear(cmp_func_t cmp){
-    heap_t* heap = malloc(sizeof(heap_t));
-    if (heap == NULL){
-        return NULL;
-    }
-    heap->valores = malloc(sizeof(void*) * TAMANIO_INICIAL);
-    if(heap->valores == NULL){
-        free(heap);
-        return NULL;
-    }
-    heap->capacidad = TAMANIO_INICIAL;
-    heap->cantida = 0;
-    heap->funcion_comparar = cmp;
-    return heap;
-}
-
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
     heap_t* heap = malloc(sizeof(heap_t));
     if (heap == NULL){
@@ -53,28 +39,37 @@ heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
         free(heap);
         return NULL;
     }
+
     for (size_t i = 0; i < n; i++){
-        heap->valores = arreglo[i];
+        heap->valores[i] = arreglo[i];
     }
     heap->capacidad = n;
-    heap->cantida = n;
+    heap->cantidad = (arreglo != NULL) ? n : 0;
     heap->funcion_comparar = cmp;
-    
-    for (size_t i = n; i != 0; i--){
-        downheap(heap, i);
+    if ( arreglo != NULL ) { 
+	for (size_t i = 0; i < n; i++) {
+	    heap->valores[i] = arreglo[i];
+	}
+// no es necesario pasar por los "nodos" hojas, aplico dowheap solo a los primeros n/2-1 elementos
+        for (size_t i = n/2; i != 0; i--){
+            downheap(heap,i - 1);
+        }
     }
-    
     return heap;
 }
 
+heap_t *heap_crear(cmp_func_t cmp){
+	return heap_crear_arr(NULL,TAMANIO_INICIAL,cmp);
+}
+
 void heap_destruir(heap_t *heap, void (*destruir_elemento)(void *e)){
-    for (size_t i = 0; i < heap->cantida; i++){
-        if(destruir_elemento != NULL){
-            destruir_elemento(heap->valores[i]);
-        }
-    }
-    free(heap->valores);
-    free(heap);
+	if ( destruir_elemento != NULL) {
+		for (size_t i = 0; i < heap->cantidad; i++) {
+			destruir_elemento(heap->valores[i]);
+		}
+	}
+	free(heap->valores);
+	free(heap);
 }
 
 size_t heap_cantidad(const heap_t *heap){
@@ -82,10 +77,7 @@ size_t heap_cantidad(const heap_t *heap){
 }
 
 bool heap_esta_vacio(const heap_t *heap){
-    if(heap_cantidad(heap) == 0){
-        return true;
-    }
-    return false;
+	return (heap_cantidad == 0) ? true : false;
 }
 
 
@@ -127,23 +119,32 @@ void *heap_ver_max(const heap_t *heap){
     return (heap_esta_vacio(heap)? NULL : heap->valores[MAXIMO]);
 }
 
-void* max(cmp_func_t cmp, void* padre, void* hijo_izq, void* hijo_der){
-    
+size_t max(heap_t heap, size_t pos_padre, size_t pos_hijo){
+	return (cmp(heap->valores[pos_padre],heap->valores[pos_hijo]) > 0)) ? pos_padre : pos_hijo;
 }
 
 
-void downheap(heap_t* heap, size_t padre){
-    if(padre+1 > heap->cantida){
-        return;
-    }
-    size_t pos_hijo_izq = (2*padre) + 1;
-    size_t pos_hijo_der = (2*padre) + 2;
-    void* maximo = max(heap->funcion_comparar, heap->valores[padre], heap->valores[pos_hijo_izq], heap->valores[pos_hijo_der]);
-    if(maximo != heap->valores[padre]){
-        swap(maximo, heap->valores[padre]);
-        downheap(heap, maximo);
-    }
-
+void downheap(heap_t* heap, size_t pos_padre){
+// pregunto si la posicion actual esta dentro de la primera mitad del vector, entonces descarto los "nodos" sin hijos
+	if(pos_padre >= heap->cantida / 2){
+       		return;
+    	}
+    	size_t pos_hijo_izq = (2 * pos_padre) + 1;
+    	size_t pos_hijo_der = (2 * pos_padre) + 2;
+	size_t pos_maximo;
+// pregunta el maximo entre los 2 hijos, encaso que tenga los 2
+	if (pos_hijo_izq < heap->cantidad && pos_hijo_der < heap->cantidad) {
+		pos_maximo = max(heap,pos_hijo_izq,pos_hijo_der);
+	}
+// como es un arbol izquierdista, la unica opcion en este punto es que tenga un hijo izq
+	else {
+		pos_maximo = pos_hijo_izq;
+	}
+// si lo que hay en pos_maximo es mayor a lo que hay en pos_padre los cambio
+    	if (heap->funcion_comparar(heap->valores[pos_maximo],heap->valores[padre]) > 0) {
+		swap(heap->valores,pos_maximo,pos_padre);
+		dowheap(heap,pos_maximo);
+	}
 }
 
 void *heap_desencolar(heap_t *heap){
