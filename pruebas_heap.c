4209fa43ca658899296252725a9f5412ac51592e
PedroEtche <102001298+PedroEtche@users.noreply.github.com>
2022-06-07 22:27:32 -0300

    Update pruebas_heap.c

--- pruebas_heap.c
+++ pruebas_heap.c
@@ -4,11 +4,17 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+int wrapper_strcmp(const void* x, const void* y){
+    int i = strcmp((const char*)x, (const char*)y);
+    return i;
+}
+
 static void prueba_heap_vacio()
 {
     printf("INICIO DE PRUEBAS CON HEAP VACIA\n");
 
-    heap_t* heap = heap_crear(strcmp);
+    heap_t* heap = heap_crear(wrapper_strcmp);
 
     print_test("Heap esta vacio", heap_esta_vacio(heap) == true);
     print_test("Heap tiene 0 elementos", heap_cantidad(heap) == 0);
@@ -22,7 +28,7 @@ static void prueba_heap_encolar_elementos()
 {
     printf("INICIO DE PRUEBAS ENCOLAR ELEMENTOS EN HEAP\n");
 
-    heap_t* heap = heap_crear(strcmp);  
+    heap_t* heap = heap_crear(wrapper_strcmp);  
 
     char* elem1 = "a";
     char* elem2 = "n";
@@ -30,30 +36,30 @@ static void prueba_heap_encolar_elementos()
     char* elem4 = "u";
     char* elem5 = "x";
 
-    print_test("Encolar elem1 es valido", heap_encolar(heap, elem1) == true);
+    print_test("Encolar a es valido", heap_encolar(heap, elem1) == true);
     print_test("Heap tiene 1 elemento", heap_cantidad(heap) == 1);
-    print_test("Ver el maximo devuelve a", heap_ver_max(heap) == "a");
-    print_test("Encolar elem2 es valido", heap_encolar(heap, elem2) == true);
+    print_test("Ver el maximo devuelve a", strcmp(heap_ver_max(heap),"a") == 0);
+    print_test("Encolar n es valido", heap_encolar(heap, elem2) == true);
     print_test("Heap tiene 2 elementos", heap_cantidad(heap) == 2);
-    print_test("Ver el maximo devuelve n", heap_ver_max(heap) == "n");
-    print_test("Encolar elem3 es valido", heap_encolar(heap, elem3) == true);
+    print_test("Ver el maximo devuelve n", strcmp(heap_ver_max(heap),"n") == 0);
+    print_test("Encolar e es valido", heap_encolar(heap, elem3) == true);
     print_test("Heap tiene 3 elementos", heap_cantidad(heap) == 3);
-    print_test("Ver el maximo devuelve n", heap_ver_max(heap) == "n");
-    print_test("Encolar elem4 es valido", heap_encolar(heap, elem4) == true);
+    print_test("Ver el maximo devuelve n", strcmp(heap_ver_max(heap),"n") == 0);
+    print_test("Encolar u es valido", heap_encolar(heap, elem4) == true);
     print_test("Heap tiene 4 elementos", heap_cantidad(heap) == 4);
-    print_test("Ver el maximo devuelve u", heap_ver_max(heap) == "u");
-    print_test("Encolar elem5 es valido", heap_encolar(heap, elem5) == true);
+    print_test("Ver el maximo devuelve u", strcmp(heap_ver_max(heap),"u") == 0);
+    print_test("Encolar x es valido", heap_encolar(heap, elem5) == true);
     print_test("Heap tiene 5 elementos", heap_cantidad(heap) == 5);
-    print_test("Ver el maximo devuelve x", heap_ver_max(heap) == "x");
+    print_test("Ver el maximo devuelve x", strcmp(heap_ver_max(heap),"x") == 0);
 
     heap_destruir(heap, NULL); 
 }
 
 static void prueba_heap_desencolar_elementos()
 {
-    printf("INICIO DE PRUEBAS ENCOLAR ELEMENTOS EN HEAP\n");
+    printf("INICIO DE PRUEBAS DESENCOLAR ELEMENTOS EN HEAP\n");
 
-    heap_t* heap = heap_crear(strcmp);  
+    heap_t* heap = heap_crear(wrapper_strcmp);  
 
     char* elem1 = "n";
     char* elem2 = "a";
@@ -64,14 +70,14 @@ static void prueba_heap_desencolar_elementos()
     print_test("Encolar elem2 es valido", heap_encolar(heap, elem2) == true);
     print_test("Encolar elem3 es valido", heap_encolar(heap, elem3) == true);
     print_test("Encolar elem4 es valido", heap_encolar(heap, elem4) == true);
-    print_test("Ver el maximo devuelve u", heap_ver_max(heap) == "u");
-    print_test("Desencolar en el heap devuelve u", heap_desencolar(heap) == "u");
-    print_test("Ver el maximo devuelve n", heap_ver_max(heap) == "n");
-    print_test("Desencolar en el heap devuelve n", heap_desencolar(heap) == "n");
-    print_test("Ver el maximo devuelve e", heap_ver_max(heap) == "e");
-    print_test("Desencolar en el heap devuelve e", heap_desencolar(heap) == "e");
-    print_test("Ver el maximo devuelve a", heap_ver_max(heap) == "a");
-    print_test("Desencolar en el heap devuelve a", heap_desencolar(heap) == "a");
+    print_test("Ver el maximo devuelve u", strcmp(heap_ver_max(heap),"u") == 0);
+    print_test("Desencolar en el heap devuelve u", strcmp(heap_desencolar(heap),"u") == 0);
+    print_test("Ver el maximo devuelve n", strcmp(heap_ver_max(heap),"n") == 0);
+    print_test("Desencolar en el heap devuelve n", strcmp(heap_desencolar(heap),"n") == 0);
+    print_test("Ver el maximo devuelve e", strcmp(heap_ver_max(heap),"e") == 0);
+    print_test("Desencolar en el heap devuelve e", strcmp(heap_desencolar(heap),"e") == 0);
+    print_test("Ver el maximo devuelve a", strcmp(heap_ver_max(heap),"a") == 0);
+    print_test("Desencolar en el heap devuelve a", strcmp(heap_desencolar(heap),"a") == 0);
     print_test("Desencolar en el heap devuelve NULL", heap_desencolar(heap) == NULL);
 
     heap_destruir(heap, NULL); 
@@ -81,24 +87,25 @@ static void prueba_heap_mantiene_invariante()
 {
     printf("INICIO DE PRUEBAS HEAP MANTIENE INVARIANTE\n");
 
-    heap_t* heap = heap_crear(strcmp);  
+    heap_t* heap = heap_crear(wrapper_strcmp);  
 
     char* elem1 = "n";
     char* elem2 = "a";
     char* elem3 = "e";
 
-    print_test("Encolar elem1 es valido", heap_encolar(heap, elem1) == true);
-    print_test("Encolar elem2 es valido", heap_encolar(heap, elem2) == true);
-    print_test("Encolar elem3 es valido", heap_encolar(heap, elem3) == true);
-    print_test("Desencolar en el heap devuelve n", heap_desencolar(heap) == "n");
-    print_test("Encolar elem1 es valido", heap_encolar(heap, elem1) == true);
-    print_test("Ver el maximo devuelve n nuevamente", heap_ver_max(heap) == "n");
-    print_test("Desencolar en el heap devuelve e", heap_desencolar(heap) == "e");
-    print_test("Desencolar en el heap devuelve a", heap_desencolar(heap) == "a");
-    print_test("Heap esta vacio", heap_esta_vacio(heap) == true);
-    print_test("Encolar elem1 es valido", heap_encolar(heap, elem1) == true);
-    print_test("Encolar elem2 es valido", heap_encolar(heap, elem2) == true);
-    print_test("Ver el maximo devuelve n", heap_ver_max(heap) == "n");
+    print_test("Encolar n es valido", heap_encolar(heap, elem1) == true);
+    print_test("Encolar a es valido", heap_encolar(heap, elem2) == true);
+    print_test("Encolar e es valido", heap_encolar(heap, elem3) == true);
+    print_test("Desencolar en el heap devuelve n", strcmp(heap_desencolar(heap),"n") == 0);
+    print_test("Encolar n es valido", heap_encolar(heap, elem1) == true);
+    print_test("Ver el maximo devuelve n nuevamente", strcmp(heap_ver_max(heap),"n") == 0);
+    printf("%s\n", (char*)heap_ver_max(heap));
+    print_test("Desencolar en el heap devuelve n", strcmp(heap_desencolar(heap),"n") == 0);
+    print_test("Desencolar en el heap devuelve e", strcmp(heap_desencolar(heap),"e") == 0);
+    print_test("Heap no esta vacio", heap_esta_vacio(heap) == false);
+    print_test("Encolar n es valido", heap_encolar(heap, elem1) == true);
+    print_test("Encolar e es valido", heap_encolar(heap, elem3) == true);
+    print_test("Ver el maximo devuelve n", strcmp(heap_ver_max(heap),"n") == 0);
     print_test("Heap no esta vacio", heap_esta_vacio(heap) == false);
 
     heap_destruir(heap, NULL); 
@@ -106,9 +113,9 @@ static void prueba_heap_mantiene_invariante()
 
 static void prueba_heap_elementos_dinamicos()
 {
-    printf("INICIO DE PRUEBAS HEAP MANTIENE INVARIANTE\n");
+    printf("INICIO DE PRUEBAS HEAP CON ELEMENTOS DINAMICOS\n");
 
-    heap_t* heap = heap_crear(strcmp);
+    heap_t* heap = heap_crear(wrapper_strcmp);
     
     char* elem1 = malloc(sizeof(char*));
     char* elem2 = malloc(sizeof(char*));
@@ -121,28 +128,28 @@ static void prueba_heap_elementos_dinamicos()
     print_test("Encolar elem2 es valido", heap_encolar(heap, elem2) == true);
     print_test("Encolar elem3 es valido", heap_encolar(heap, elem3) == true);
     print_test("Heap tiene 3 elementos", heap_cantidad(heap) == 3);
-    print_test("Ver el maximo devuelve n", heap_ver_max(heap) == "n");
-    print_test("Desencolar en el heap devuelve n", heap_desencolar(heap) == "n");
+    print_test("Ver el maximo devuelve n", strcmp(heap_ver_max(heap),"n") == 0);
+    print_test("Desencolar en el heap devuelve n", strcmp(heap_desencolar(heap),"n") == 0);
     print_test("Heap tiene 2 elementos", heap_cantidad(heap) == 2);
 
     heap_destruir(heap, free); 
 }
 
-int comparar(int x, int y){
-    if(x < y){
+int comparar(const void* x, const void* y){
+    if(*(int*)x < *(int*)y){
         return -1;
-    }else if(x == y){
+    }else if(*(int*)x == *(int*)y){
         return 0;
     }
     return 1;
 }
 
-static void prueba_heap_volumen()
+static void prueba_heap_volumen(size_t tam)
 {
     printf("INICIO DE PRUEBAS HEAP VOLUMEN\n");
 
     heap_t* heap = heap_crear(comparar);
-    size_t tam = 1000;
+
 
     int numeros[tam];
     for (int i = 0; i < tam; i++){
@@ -155,18 +162,15 @@ static void prueba_heap_volumen()
         ok &= heap_encolar(heap, &numeros[i]);
     }
     print_test("Se pudieron encolar todos los elementos", ok);
+    print_test("Hay el tamaño deseado de elementos", heap_cantidad(heap) == tam);
 
     ok = true;
-    int valor;
     for (; i > 0; i--) {
-        valor = *(int*)heap_desencolar(heap);
-        if(heap_esta_vacio(heap)){
-            ok = false;
-        }
+        heap_desencolar(heap);
         ok &= heap_cantidad(heap) == (i - 1);
     }
     print_test("Se pudieron desapilar y obtener todos los elementos", ok);
-    print_test("Veo que la pila quedo vacia", heap_esta_vacio(heap) == true);
+    print_test("Veo que el heap quedo vacio", heap_esta_vacio(heap) == true);
     print_test("Veo que el maximo del heap", heap_ver_max(heap) == NULL);
     print_test("Desencolar de nuevo me da NULL", heap_desencolar(heap) == NULL);
 
@@ -179,27 +183,41 @@ static void prueba_heap_volumen()
     heap_destruir(heap, NULL);
 }
 
-void mezclar(int arr[], size_t largo){
+void mezclar(void* arr[], size_t largo){
     for (size_t i = 0; i < largo; i++){
-        int indice_aux = rand() % largo;
-        int temporal = arr[i];
+        size_t indice_aux = (size_t)rand() % largo;
+        void* temporal = arr[i];
         arr[i] = arr[indice_aux];
         arr[indice_aux] = temporal;
     }
 }
 
-
-static void prueba_heap_heapify()
+static void prueba_heap_sort()
 {
     printf("INICIO DE PRUEBAS HEAPIFY\n");
 
-    int largo = 10;
-    int arr[largo];
-    for (size_t i = 0; i < largo; i++){
-        arr[i] = i;
+    size_t largo = 10;
+    int numeros[largo];
+    for (int i = 0; i < largo; i++)
+    {
+        numeros[i] = i;
+    }
+    void* arr[10];
+    for (int i = 0; i < largo; i++)
+    {
+        arr[i] = &numeros[i];
     }
     
     mezclar(arr, largo);
+    
+    for (size_t i = 0; i < largo-1; i++){
+        printf("%i\n", *(int*)arr[i]);
+    }
+    heap_sort(arr, largo, comparar);
+    printf("Luego de heap sort\n");
+    for (size_t i = 0; i < largo-1; i++){
+        printf("%i\n", *(int*)arr[i]);
+    }
     
     bool ok = true;
     for (size_t i = 0; i < largo-1; i++){
@@ -208,38 +226,39 @@ static void prueba_heap_heapify()
             break;
         }
     }
-    print_test("El array esta arreglado", ok);
+    print_test("El array esta ordenado", ok);
 }
 
 static void prueba_heap_a_partir_de_arreglo()
 {
     printf("INICIO DE PRUEBAS CREAR HEAP A PARTIT DE ARREGLO\n");
 
-    int largo = 10;
-    int arr[largo];
-    for (size_t i = 0; i < largo; i++){
-        arr[i] = i;
+
+    size_t largo = 10;
+    int numeros[largo];
+    for (int i = 0; i < largo; i++)
+    {
+        numeros[i] = i;
+    }
+    void* arr[10];
+    for (int i = 0; i < largo; i++)
+    {
+        arr[i] = &numeros[i];
     }
     
-    mezclar(arr, largo);
-
-    heap_t* heap = heap_crear_arr(arr, largo, comparar);
-
-    int elem1 = 1;
-    int elem2 = 3;
-    int elem3 = 2;
 
 
+    heap_t* heap = heap_crear_arr(arr, 10, comparar);
+
+    int elem1 = 100;
+
+    print_test("Ver el maximo devuelve 9", *(int*)heap_ver_max(heap) == 9);
     print_test("Encolar elem1 es valido", heap_encolar(heap, &elem1) == true);
-    print_test("Heap tiene 1 elemento", heap_cantidad(heap) == 1);
-    print_test("Ver el maximo devuelve 1", *(int*)heap_ver_max(heap) == 1);
-    print_test("Encolar elem2 es valido", heap_encolar(heap, &elem2) == true);
-    print_test("Heap tiene 2 elementos", heap_cantidad(heap) == 2);
-    print_test("Ver el maximo devuelve 3", *(int*)heap_ver_max(heap) == 3);
-    print_test("Encolar elem3 es valido", heap_encolar(heap, &elem3) == true);
-    print_test("Heap tiene 3 elementos", heap_cantidad(heap) == 3);
-    print_test("Ver el maximo devuelve 3", *(int*)heap_ver_max(heap) == 3);
-    print_test("Desencolar devuelve 3", *(int*)heap_desencolar(heap) == 3);
+    print_test("Ver el maximo devuelve 100", *(int*)heap_ver_max(heap) == 100);
+    print_test("Heap tiene 2 elementos", heap_cantidad(heap) == 11);
+    print_test("Desencolar devuelve 100", *(int*)heap_desencolar(heap) == 100);
+    print_test("Desencolar devuelve 9", *(int*)heap_desencolar(heap) == 9);
+    print_test("Desencolar devuelve 8", *(int*)heap_desencolar(heap) == 8);
 
     heap_destruir(heap, NULL); 
 }
@@ -251,7 +270,13 @@ void pruebas_heap_estudiante()
     prueba_heap_desencolar_elementos();
     prueba_heap_mantiene_invariante();
     prueba_heap_elementos_dinamicos();
-    prueba_heap_volumen();
-    prueba_heap_heapify();
+    prueba_heap_volumen(10000);
+    prueba_heap_sort();
     prueba_heap_a_partir_de_arreglo();
 }
+
+
+// void pruebas_volumen_catedra(size_t largo)
+// {
+//     prueba_heap_volumen(largo);
+// }
